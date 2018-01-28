bde3ae81847b6f1ae8700c159c6b3f6f08972ebb
KidEinstein <anirudhshekh@gmail.com>
2018-01-28 13:17:35 -0800

    Add logging

--- test/basic_tests/prime_number_check.cpp
+++ test/basic_tests/prime_number_check.cpp
@@ -4,13 +4,13 @@
 
 #include "gtest/gtest.h"
 #include "PrimeNumber.h"
-
+#include "logger.hpp"
 TEST(primenumber_tests, validnumber) {
 
   EXPECT_EQ(5, PrimeNumber(3));
   EXPECT_EQ(17, PrimeNumber(7));
   EXPECT_EQ(31, PrimeNumber(11));
-  std::cout << "Current Points:10/20" << std::endl;
+  Logger::getInstance().logScore("Prime numbers", 10, 15);
 }
 
 
@@ -18,5 +18,5 @@ TEST(primenumber_tests, invalidnumber) {
 
   EXPECT_EQ(-1, PrimeNumber(0));
   EXPECT_EQ(-1, PrimeNumber(-2));
-
+  Logger::getInstance().logScore("Prime for negative numbers", 20, 20);
 }
